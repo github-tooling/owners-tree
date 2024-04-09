212a42ca636f861f0f4a1db330ad89f26e394562
Andrii Oriekhov <andriyorehov@gmail.com>
2024-04-09 23:51:33 +0300

    change action

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
+const fs = require('fs');
 
-import { getTreeByFile } from '@andriyorehov/ts-graph';
+const tsGraph = require('@andriyorehov/ts-graph');
 
 const owners = fs.readFileSync('owners.json', 'utf8');
 const ownersObj = JSON.parse(owners);
@@ -8,6 +8,6 @@ const ownersObj = JSON.parse(owners);
 for (const owner of ownersObj) {
   for (const entries of owner.owns) {
     console.log(entries);
-    console.log(Object.keys(getTreeByFile(entries).flatTree));
+    console.log(Object.keys(tsGraph.getTreeByFile(entries).flatTree));
   }
 }
